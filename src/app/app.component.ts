d94b9744ac2fd02d59bc1aaf11c076436c330d48
vg <vg2@ukrosoft.com.ua>
2018-11-12 16:49:45 +0200

    Start + header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  componentTitle = 'cjmponent title';
+
+  clickHandler (){
+    alert('Отакои');
+  }
 }
+
+
