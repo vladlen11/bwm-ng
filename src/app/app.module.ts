8e76f640286763e508bb1ce82888c8fd01a8b480
vg <vg2@ukrosoft.com.ua>
2018-11-12 18:22:26 +0200

    item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
 
-
+const routes: Routes = [
+    {path:'', component: RentalComponent},
+    {path:'temp', component: TempComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentalComponent,
+    TempComponent,
+    RentalListComponent,
+    RentalListItemComponent
   ],
   imports: [
+      RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
